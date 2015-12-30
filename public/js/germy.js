605a9414ccaa69660cd11ed95ef06b5d45a2ecb9
Qingcai <1016903103@qq.com>
2015-12-31 01:12:51 +0800

    baidu map

--- public/js/germy.js
+++ public/js/germy.js
@@ -67,10 +67,81 @@ $(function() {
             });
         });
     }
-
     if ($('#order-preview').length > 0) {
         $('#order-preview').on('click', '.ui-icon-close', function() {
-           $(this).siblings('input').val('');
+            $(this).siblings('input').val('');
         });
+    }
+    if ($('#map').length > 0) {
+        var map = new BMap.Map("map");
+        point = new BMap.Point(116.404, 39.915);
+        var lng, lat;
+        var province, city, district, street;
+        map.centerAndZoom(point, 12);
+        var getlocation = new BMap.Geolocation();
+        getlocation.getCurrentPosition(function(e) {
+            if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+                lng = e.point.lng;
+                lat = e.point.lat;
+                var marker = new BMap.Marker(e.point);
+                map.addOverlay(marker);
+                map.panTo(e.point);
+                marker.enableDragging();
+                var geo = new BMap.Geocoder();
+                geo.getLocation(e.point, function(rs) {
+                    var addComp = rs.addressComponents;
+                    province = addComp.province;
+                    city = addComp.city;
+                    district = addComp.district;
+                    street = addComp.street;
+                    updateLocationInfo();
+                });
+                marker.addEventListener("dragend", function(e) {
+                    lng = e.point.lng;
+                    lat = e.point.lat;
+                    geo.getLocation(e.point, function(rs) {
+                        var addComp = rs.addressComponents;
+                        province = addComp.province;
+                        city = addComp.city;
+                        district = addComp.district;
+                        street = addComp.street;
+                        updateLocationInfo();
+                    });
+                });
+            }
+            else {
+                alert('定位失败，请重试' + this.getStatus());
+            }
+        }, {enableHighAccuracy: true})
+        var address = '';
+        var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+        // 添加带有定位的导航控件
+        var navigationControl = new BMap.NavigationControl({
+            // 靠左上角位置
+            anchor: BMAP_ANCHOR_TOP_LEFT,
+            // LARGE类型
+            type: BMAP_NAVIGATION_CONTROL_LARGE,
+            // 启用显示定位
+            enableGeolocation: true
+        });
+        map.addControl(navigationControl);
+        map.addControl(top_left_control);
+
+
+        function updateLocationInfo() {
+            $('#location-info').html('');
+            $('<input>').attr({
+                'type': 'hidden',
+                'value': lng,
+                'name': 'lng',
+            }).appendTo($('#location-info'));
+            $('<input>').attr({
+                'type': 'hidden',
+                'value': lat,
+                'name': 'lat',
+            }).appendTo($('#location-info'));
+            $('input[name="location"]').val(province+city+district+street);
+        }
+
     }
 });
--- resources/views/order/preview.blade.php
+++ resources/views/order/preview.blade.php
@@ -2,72 +2,80 @@
 
 @section('content')
     <div id="order-preview">
-            {!! Form::open(['url' => url('order/submit'), 'id' => 'order-submit-form']) !!}
-            <p class="title">点餐信息确认</p>
-            <div class="bought-items">
-                @foreach($items as $item)
-                    <div class="ui-row-flex ui-whitespace item">
-                        <div class="ui-col ui-col-4 name">{{ $item['name'] }}</div>
-                        <div class="ui-col ui-col-1 price-info"><i class="fa fa-cny"></i><span
-                                    class="price">{{ $item['price'] }}</span></div>
-                        <div class="ui-col ui-col-1 number-info"><span class="number">{{ $item['number'] }}</span><span
-                                    class="unit">{{ $item['unit'] }}</span>
-                        </div>
-                        {!! Form::hidden('order[][index]', $item['index']) !!}
+        <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=AE4d81c6a9edc1562e49ccc0a87d3fb5"></script>
+        {!! Form::open(['url' => url('order/submit'), 'id' => 'order-submit-form']) !!}
+        <p class="title">点餐信息确认</p>
+
+        <div class="bought-items">
+            @foreach($items as $item)
+                <div class="ui-row-flex ui-whitespace item">
+                    <div class="ui-col ui-col-4 name">{{ $item['name'] }}</div>
+                    <div class="ui-col ui-col-1 price-info"><i class="fa fa-cny"></i><span
+                                class="price">{{ $item['price'] }}</span></div>
+                    <div class="ui-col ui-col-1 number-info"><span class="number">{{ $item['number'] }}</span><span
+                                class="unit">{{ $item['unit'] }}</span>
                     </div>
-                @endforeach
-            </div>
-            <div class="ui-row-flex ui-whitespace total-info">
-                <div class="ui-col ui-col-4 total">总计</div>
-                <div class="ui-col ui-col-1 price-info">
-                    <i class="fa fa-cny"></i>
-                    <span class="price">{{ $total['price'] }}</span>
+                    {!! Form::hidden('order[][index]', $item['index']) !!}
                 </div>
-                <div class="ui-col ui-col-1 number-info">
-                    <span class="number">{{ $total['number'] }}</span>
-                </div>
+            @endforeach
+        </div>
+        <div class="ui-row-flex ui-whitespace total-info">
+            <div class="ui-col ui-col-4 total">总计</div>
+            <div class="ui-col ui-col-1 price-info">
+                <i class="fa fa-cny"></i>
+                <span class="price">{{ $total['price'] }}</span>
             </div>
-            <p class="title">个人信息</p>
+            <div class="ui-col ui-col-1 number-info">
+                <span class="number">{{ $total['number'] }}</span>
+            </div>
+        </div>
+        <p class="title">个人信息</p>
 
-            <div class="ui-form ui-border-t">
+        <div class="ui-form ui-border-t" id="map">
 
-                <div class="ui-form-item ui-form-item-l ui-border-b">
-                    <label class="ui-border-r">
-                        姓名
-                    </label>
-                    <input type="text" placeholder="请填写您的姓名"/>
-                    <a class="ui-icon-close">
-                    </a>
-                </div>
-                <div class="ui-form-item ui-form-item-l ui-border-b">
-                    <label class="ui-border-r">
-                        备注
-                    </label>
-                    <input type="text" placeholder="写下您想说的话"/>
-                    <a class="ui-icon-close">
-                    </a>
-                </div>
-                <div class="ui-form-item ui-form-item-l ui-border-b">
-                    <label class="ui-border-r">
-                        地址
-                    </label>
-                    <input type="text" placeholder="请填写街道详细地址"/>
-                    <a class="ui-icon-close"></a>
-                </div>
-                <div class="ui-form-item ui-form-item-l ui-border-b">
-                    <label class="ui-border-r">
-                        手机
-                    </label>
-                    <input type="text" placeholder="请输入手机号码"/>
-                    <a class="ui-icon-close">
-                    </a>
-                </div>
-                <div class="ui-btn-wrap">
-                    <button class="ui-btn-lg ui-btn-danger">
-                        提交订单
-                    </button>
-                </div>
+        </div>
+        <div id="location-info">
+
+        </div>
+        <div class="ui-form ui-border-t">
+
+            <div class="ui-form-item ui-form-item-l ui-border-b">
+                <label class="ui-border-r">
+                    姓名
+                </label>
+                <input type="text" placeholder="请填写您的姓名"/>
+                <a class="ui-icon-close">
+                </a>
             </div>
+            <div class="ui-form-item ui-form-item-l ui-border-b">
+                <label class="ui-border-r">
+                    备注
+                </label>
+                <input type="text" placeholder="写下您想说的话"/>
+                <a class="ui-icon-close">
+                </a>
+            </div>
+            <div class="ui-form-item ui-form-item-l ui-border-b">
+                <label class="ui-border-r">
+                    地址
+                </label>
+                <input type="text" name="location" placeholder="请填写街道详细地址"/>
+                <a class="ui-icon-close"></a>
+            </div>
+            <div class="ui-form-item ui-form-item-l ui-border-b">
+                <label class="ui-border-r">
+                    手机
+                </label>
+                <input type="text" placeholder="请输入手机号码"/>
+                <a class="ui-icon-close">
+                </a>
+            </div>
+            <div class="ui-btn-wrap">
+                <button class="ui-btn-lg ui-btn-danger">
+                    提交订单
+                </button>
+            </div>
+        </div>
         {!! Form::close() !!}
     </div>
 @endsection
